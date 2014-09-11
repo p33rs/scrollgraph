d94c2346afb9966e70eb99cf58d197d301924707
Jon <hello@jonpierce.net>
2014-09-11 18:57:01 -0400

    fwk

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8" />
+    <meta name="description" content="[c]" />
+    <title>[c]</title>
+
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            font-family: 'Arial Black', sans-serif;
+            font-size: 1.5em;
+        }
+    </style>
+
+</head>
+<body>
+http://fiddle.jshell.net/c2mru/8/
+</body>
+</html>
---
+++ src/Bandwidth.php
@@ -0,0 +1,14 @@
+<?php
+namespace Capacity\Graph;
+
+/**
+ * Generates the bandwidth information.
+ * The bandwidth information is fake.
+ */
+class Bandwidth {
+
+  public function get() {
+
+  }
+
+}
